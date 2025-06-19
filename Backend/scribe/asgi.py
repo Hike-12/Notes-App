053f098d2c8f213d2532c4f3a1cab53b15ca225f
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-06-19 19:38:10 +0530

    Add a blank line for improved readability in asgi.py

--- Backend/scribe/asgi.py
+++ Backend/scribe/asgi.py
@@ -5,6 +5,7 @@ from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scribe.settings')
 
+
 # Import Django before importing anything else that might use Django models
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
