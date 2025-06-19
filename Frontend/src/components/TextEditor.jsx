0f52b7419fafa6996e4be6146b301bdc5de4f47d
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-06-19 17:57:59 +0530

    blah

--- Frontend/src/components/TextEditor.jsx
+++ Frontend/src/components/TextEditor.jsx
@@ -221,7 +221,7 @@ export default function TextEditor({ setSidebarOpen }) {
             >
               {isSaving ? (
                 <>
-                  <div className="animate-spin rounded-full h-3 h-3 sm:h-4 sm:w-4 border-b-2 border-white mr-1 sm:mr-2"></div>
+                  <div className="animate-spin rounded-full h-3 sm:h-4 sm:w-4 border-b-2 border-white mr-1 sm:mr-2"></div>
                   <span className="hidden sm:inline">Saving...</span>
                   <span className="sm:hidden">Save</span>
                 </>
