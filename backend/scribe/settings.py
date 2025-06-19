f7e4bbd51c064a7c2fd3192daf1e52bd6435af4a
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-06-19 20:50:32 +0530

    Refactor CHANNEL_LAYERS configuration to use environment variables for Redis host, port, and password

--- backend/scribe/settings.py
+++ backend/scribe/settings.py
@@ -182,14 +182,11 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [
-                {
-                    "address": os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379'),
-                    "connection_kwargs": {
-                        "ssl_cert_reqs": None,
-                    },
-                }
-            ] if os.environ.get('REDIS_URL') else ['redis://127.0.0.1:6379'],
+            "hosts": [(
+                os.environ.get('REDIS_HOST', '127.0.0.1'), 
+                int(os.environ.get('REDIS_PORT', 6379))
+            )],
+            "password": os.environ.get('REDIS_PASSWORD'),
         },
     },
 }
