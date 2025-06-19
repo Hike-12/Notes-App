8cf0cb8e388fda498c235c5a594dd53d967c0526
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-06-19 19:14:08 +0530

    Add static file settings and update WebSocket connection logic; include build script and requirements

--- Backend/scribe/settings.py
+++ Backend/scribe/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Add this if you have a static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
